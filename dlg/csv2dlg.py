7da843614ccb6e4bdb364aebb9123273e399a9a0
lbreede <67719520+lbreede@users.noreply.github.com>
2024-12-30 13:57:54 -0800

    updated (soon to be obsolete) regex pattern

--- dlg/csv2dlg.py
+++ dlg/csv2dlg.py
@@ -1,13 +1,15 @@
 import os.path
 import re
 import sys
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
 def main():
     file = sys.argv[1]
     root, _ = os.path.splitext(file)
 
-    pattern = re.compile(r"^\{(\d+)}\{(.+)}\{(.*)}\{(\d*)}\{(\w*)}\{(\d*)}\{(\w*)}")
+    pattern = re.compile(r"^\{(\d+)}\{(.+)}\{(.*)}\{(\d*)}\{(.*)}\{(-?\d*)}\{(.*)}")
 
     line_groups: list[tuple[str,...]] = []
     group_widths: list[int] = [0,0,0,0,0,0,0]
@@ -16,8 +18,11 @@ def main():
         print(f"File {file!r} not found.")
         return
 
-    with open(file, "r", encoding="utf-8") as fp:
-        for line in fp:
+    with (open(file, "r", encoding="utf-8") as fp):
+        for line in fp.read().splitlines():
+            if not line:
+                continue
+            logging.debug(line)
             if match := pattern.match(line):
                 groups = match.groups()
 
@@ -25,6 +30,9 @@ def main():
                     group_widths[i] = max(group_widths[i], len(groups[i]))
 
                 line_groups.append(match.groups())
+                logging.info("Successfully parsed line")
+            else:
+                logging.warning("Could not parse line")
 
     s = ""
     for group in line_groups:
@@ -32,7 +40,7 @@ def main():
         s += " }{ ".join(g.rjust(w) if g.isdigit() else g.ljust(w) for g, w in zip(group, group_widths))
         s += " }\n"
 
-    file = root + ".dlg"
+    file = root + "_formatted.dlg"
     with open(file, "w", encoding="utf-8") as fp:
         fp.write(s)
 
